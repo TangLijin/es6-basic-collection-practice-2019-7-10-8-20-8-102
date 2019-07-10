a0378149e109bb41b0de3bee04386304701343da
tanglijin <1614468806@qq.com>
2019-07-10 23:35:14 +0800

    finish section2 practice1

--- main/section-2/practice-1.js
+++ main/section-2/practice-1.js
@@ -1,5 +1,24 @@
 'use strict';
 
 module.exports = function countSameElements(collection) {
-  return '实现练习要求，并改写该行代码。';
+  let result = [];
+  //collection.sort();
+  let temp = {};
+  let firstPlace = 0;
+  let lastPlace = 0;
+  let currentNumber = collection[0];
+
+  while(lastPlace < collection.length - 1){
+    firstPlace = collection.indexOf(currentNumber);
+    lastPlace = collection.lastIndexOf(currentNumber);
+    temp.key = currentNumber;
+    temp.count = lastPlace - firstPlace + 1;
+    result.push(temp);
+    temp = {};
+
+    currentNumber = collection[lastPlace + 1];
+  }
+  console.log(result);
+  return result;
+
 }
